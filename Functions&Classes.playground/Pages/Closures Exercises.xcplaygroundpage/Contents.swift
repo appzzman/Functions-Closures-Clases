c49316e01ff7da9fa3bec925acbf6ad394ed7cfc
Janusz Chudzynski <appzzman>
2016-01-25 08:17:18 -0600

    Updated

--- Functions&Classes.playground/Pages/Closures Exercises.xcplaygroundpage/Contents.swift
+++ Functions&Classes.playground/Pages/Closures Exercises.xcplaygroundpage/Contents.swift
@@ -2,8 +2,8 @@
 
 import Foundation
 
-/*: 
- # Closures
+/*:
+# Closures
 
 Closures are self contained chunks of code that can be passed around and used in your code. Closures can capture and store references to any constants or variables from the context in which they are defined. This is know as closing over those variables, hance the name closures. Closures are use intensively in the Cocoa frameworks – which are used to develop iOS or Mac applications.
 
@@ -20,9 +20,10 @@ The thing to keep in mind for the moment is that you already have an intuition a
 // a closure that has no parameters and doesn't return anything
 func check(){
     var b = 1
-    var doSomething: () -> (Void) = {
+    let doSomething: () -> Void = {
         b++
     }
+    
     doSomething()
     b
 }
@@ -48,8 +49,8 @@ var alsoDouble = double
 alsoDouble(3) // 6
 
 
-/*: 
- ## Declaring a closure
+/*:
+## Declaring a closure
 The general syntax for declaring closures is:
 
 { (parameters) -> return type in
@@ -109,8 +110,12 @@ var multipleParametersAndReturnValue2 =
 //: # Shorthand Parameter Names
 //: Swift provides shorthand parameter names for closures. You can refer to the parameters as $0, $1, $2 and so on. To use shorthand parameter names ignore the first part of the declaration.
 
-var numbers = [95762]
-numbers.sort({ return $0 < $1 })
+var numbers = [9,5,7,6,2]
+var k = numbers.sort({ return $0 > $1 })
+
+k
+var n = numbers.sort(<)
+n
 
 var double2: (Int) -> (Int) = {
     return $0 * 2
@@ -120,8 +125,8 @@ var sum2: (Int, Int) -> (Int) = {
     return $0 + $1
 }
 
-/*: 
- ## Closures are reference types
+/*:
+## Closures are reference types
 Closures are reference types. This means that when you assign a closure to more than one variable they will refer to the same closure. This is different from value type which make a copy when you assign them to another variable or constant.
 */
 
@@ -137,25 +142,25 @@ var alsoDouble2 = double3
 alsoDouble(3) // 6
 
 
-/*: 
- ## Implicit Return Values
+/*:
+## Implicit Return Values
 Closures that have only one statement will return the result of that statement. To do that simply omit the returnkeyword.
 */
 
 let array = [4,5,19,1,4,5]
 array.sort { $0 < $1 }
 
-/*: 
- # Higher order functions
+/*:
+# Higher order functions
 A higher order function is a function that does at least one of the following:
- - takes a function as input
- - outputs a function
- - Swift has three important higher order functions implemented for arrays: map, filter and reduce.
+- takes a function as input
+- outputs a function
+- Swift has three important higher order functions implemented for arrays: map, filter and reduce.
 */
 
-/*: 
- ## Map
- Transforms an array using a function
+/*:
+## Map
+Transforms an array using a function
 
 [ x1, x2, ... , xn].map(f) -> [f(x1), f(x2), ... , f(xn)]
 Let’s take as an example the problem of converting an array of numbers to an array of strings.
@@ -164,11 +169,11 @@ Let’s take as an example the problem of converting an array of numbers to an a
 One way of solving this problem would be to create an empty array of strings, iterate over the original array transforming each element and adding it to the new one.
 */
 
- numbers = [1, 2, 3]
+numbers = [1, 2, 3]
 var strings: [String] = []
 
 for number in numbers {
-    strings.append("\(number)")
+    strings.append("Number \(number)")
 }
 
 //: The other way of solving this problem is by using map:
@@ -184,38 +189,47 @@ The closure that we need to give to map take one parameter and will be called on
 */
 
 /*:
- ## Filter
+## Filter
 
 Filter selects the elements of an array which satisfy a certain condition.
-    
+
 For example let’s remove all the odd numbers from an array:
 */
- numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 
 var evenNumbers = numbers.filter { $0 % 2 == 0 }
-// evenNumbers = [2, 4, 6, 8]
+
+evenNumbers
 
 //: Or remove all the even numbers:
 
 var oddNumbers = numbers.filter { $0 % 2 == 1 }
-// oddNumbers = [1, 3, 5, 7]
+oddNumbers
 
 /*:
 The closure that we need to give to map takes one parameter and will be called once for each of the elements from the array. It should return a Bool value, if it’s true the element will be copied into the new array otherwise no.
 */
 
 
-/* :
- ## Reduce
+/*:
+## Reduce
 
 Reduce combines the value of an array into a single value.
-    
+
 For example we can reduce an array of numbers to their sum.
 */
 
 numbers = [1, 2, 3, 4, 5]
 var sum = numbers.reduce(0) { $0 + $1 } // 15
+var sum3 = numbers.reduce(0) {(sum, element) in sum + element } // 15
 
+//Changing return type
+var r = numbers.reduce(0) {(sum, element)->Double in
+    var newsum = sum + Double(element)
+    return Double(newsum) / Double(element)
+}
+
+r
 /*:
 Reduce take two parameters, an initial value and a closure that will be used to combine the elements of the array. The closure provided to reduce takes two parameters, the first one is the partial result and the second one will be an element from the array. The closure will be called for each element once. In the sum example we started the sum from 0 and the closure added the partial sum with each element.
 
@@ -229,14 +243,14 @@ sum = numbers.reduce(0, combine: +) // 15
 //: # When you have a new hammer everything starts to look like a nail. Don't over use the functions you just learned about
 
 /*:
- 1. Write a function applyTwice(f:(Float -> Float),x:Float) -> Float that takes a function f and a float x and aplies f to x twice i.e. f(f(x))
- 2. Write a function applyKTimes(f:(Float -> Float),x:Float,k:Int) -> Float that takes a function f and a float x and aplies f to x k times
- 3. Using applyKTimes write a function that raises x to the kth power
- 4.Given an array of of dictionaries containing keys for “name” and “age” write a map function that returns an array of users created from it
- 5. Given an array of numbers write a filter method that only selects odd integers
- 6. Given an array of strings write a filter function that selects only strings that can be converted to Ints
- 7. Write a reduce function that finds the largest element in an array of Ints
- 8. Find the sum of the squares of all the odd numbers from numbers and then print it. Use map, filter and reduce to solve this problem.
+1. Write a function applyTwice(f:(Float -> Float),x:Float) -> Float that takes a function f and a float x and aplies f to x twice i.e. f(f(x))
+2. Write a function applyKTimes(f:(Float -> Float),x:Float,k:Int) -> Float that takes a function f and a float x and aplies f to x k times
+3. Using applyKTimes write a function that raises x to the kth power
+4.Given an array of of dictionaries containing keys for “name” and “age” write a map function that returns an array of users created from it
+5. Given an array of numbers write a filter method that only selects odd integers
+6. Given an array of strings write a filter function that selects only strings that can be converted to Ints
+7. Write a reduce function that finds the largest element in an array of Ints
+8. Find the sum of the squares of all the odd numbers from numbers and then print it. Use map, filter and reduce to solve this problem.
 
 */
 
